73c6124de6c061123428e32800dd573361fb0a4a
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-01-28 15:15:04 -0500

    add function with a saveBtn click listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 //Display today's day and also date 
 var todayDate = moment().format('dddd MMM Do YYYY');
 
+$(document).ready(function(){
+    //saveBtn click listener
+    $("saveBtn").on("click", function() {
+        
+
+    })
+
+
+    
+
+})
