bb10671e27ee3716569f498ef80186a747c0d043
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-01-28 15:29:51 -0500

    Third commit add values for description

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 //Display today's day and also date 
 var todayDate = moment().format('dddd MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-$(document).ready(function(){
+$(document).ready(function () {
     //saveBtn click listener
-    $("saveBtn").on("click", function() {
+    $(".saveBtn").on("click", function() {
+        //Get values of the description for Jquery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        
+
+
+
         
 
     })
