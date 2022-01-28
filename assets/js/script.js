36a9de5f262d9cb31490f833e632b03b3c7b2822
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-01-28 15:58:42 -0500

    add  loop for time blocks and cheked time and classes for background indicators

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,29 @@ $(document).ready(function () {
     function timeTracker(){
      //get current number of hours.
      var timeNow = moment().hour();   
+
+     //Create a for loop for time blocks
+     var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+     //Check the  time and add classes for background indicators
+     if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
     }
 
-    
+
+}
 
 })
